897bf1f7c1b48c9f70192d85ab8b6e839e12d1ca
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-06-10 13:51:35 -0500

    Testing async

--- spotify_parsing/main.py
+++ spotify_parsing/main.py
@@ -2,15 +2,17 @@ from pyscript import fetch
 
 print("HIHIHI")
 
-async def getSecrets():
-  response = await fetch(
-    "https://tkothenbeutel.pyscriptapps.com/divine-poetry/api/proxies/spotify-secrets",
-    method= "GET").json()
-  print(response)
+response = await fetch(
+  "https://tkothenbeutel.pyscriptapps.com/divine-poetry/api/proxies/spotify-secrets",
+  method= "GET")
+if response.ok:
+  print(await response.json())
+else:
+  print(response.status)
 
 def main():
   print("GETTING SECRETS")
-  getSecrets()
+  #getSecrets()
 
 main()
 if __name__ == "__main__":
