c8577878a02d6bb8379a79f5e5c45c575f6525b3
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-06-10 14:24:52 -0500

    Fixed link and data

--- spotify_parsing/main.py
+++ spotify_parsing/main.py
@@ -4,14 +4,10 @@ print("HIHIHI")
 
 response = await fetch(
   "https://tkothenbeutel.pyscriptapps.com/divine-poetry/api/proxies/spotify-secrets",
-  method= "GET")
-if response.ok:
-  data = await response.json()
-  for i in data:
-    print(i['id'])
-  
-else:
-  print(response.status)
+  method= "GET").json
+
+for i in response:
+  print(i['id'])
 
 def main():
   print("GETTING SECRETS")
