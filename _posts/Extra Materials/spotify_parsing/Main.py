767dc07d904d79c89cbbf23a9807b6488e89f3e3
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-06-10 09:26:30 -0500

    Changed python

--- _posts/Extra Materials/spotify_parsing/Main.py
+++ _posts/Extra Materials/spotify_parsing/Main.py
@@ -1,8 +1,18 @@
 from pyscript import fetch
 
-response = await fetch(
-    "https://tkothenbeutel.pyscriptapps.com/divine-poetry/api/proxies/spotify-secrets",
-    method= "GET"
-).json()
+print("HIHIHI")
 
-print(response)
+async def getSecrets():
+  response = await fetch(
+    "https://tkothenbeutel.pyscriptapps.com/divine-poetry/api/proxies/spotify-secrets",
+    method= "GET").json()
+  print(response)
+
+def main():
+  print("GETTING SECRETS")
+  getSecrets()
+
+main()
+if __name__ == "__main__":
+  print("In if statement!")
+  main()
