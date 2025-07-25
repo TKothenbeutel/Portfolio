558f0bac4ebd25a1635994e6423742283e4299c8
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-07-25 00:11:33 -0500

    Finished correct wrapping

--- spotify_parsing/Helpers/Formatting.py
+++ spotify_parsing/Helpers/Formatting.py
@@ -19,13 +19,65 @@ def wrap_text_to_terminal(text: str) -> str:
     width = settings.getCols()
 
     splittedText = text.splitlines()
-    #TODO:
-    '''
-    *Remove bold, save indices, add them after textwrap.fill
-    *Remove whitespace after '\n' if there's only one space after it
-    '''
-    wrapped_lines = [textwrap.fill(line,width=width, drop_whitespace=False, replace_whitespace=False) for line in splittedText]
 
+    #Temp remove bold
+    specialCharIndices = [] #True = bold; False = underline
+    boldEnd = True
+    for i in range(len(splittedText)):
+      chunk = splittedText[i]
+      specialCharIndices.append([])
+      index = len(chunk)-1
+      while('\u001b' in chunk or '\u0332' in chunk):
+        if(chunk[index] == '\u001b'):
+          #Add bold spot to list
+          specialCharIndices[-1].append((index,True))
+          #Remove bold
+          res = chunk.rsplit('\u001b[0m',1) if boldEnd else chunk.rsplit('\u001b[1m',1)
+          #Update variables
+          splittedText[i] = res[0] + res[1]
+          chunk = splittedText[i]
+          boldEnd = not boldEnd
+        elif(chunk[index] == '\u0332'):
+          #Add underline to list
+          specialCharIndices[-1].append((index,False))
+          #Remove underline
+          res = chunk.rsplit('\u0332',1)
+          #Update variables
+          splittedText[i] = res[0] + res[1]
+          chunk = splittedText[i]
+        index -= 1
+
+    wrapped_lines = [textwrap.fill(line,width=width, drop_whitespace=False, replace_whitespace=False) for line in splittedText]
+    
+    #Add in bold
+    boldEnd = False
+    for i in range(len(wrapped_lines)):
+      for charIndex in reversed(specialCharIndices[i]):
+        pos = 0
+        for j in range(len(wrapped_lines[i])+1):
+          if(pos == charIndex[0]):
+            if(charIndex[1]):
+              #Ensure \n isn't in the way
+              if(j < len(wrapped_lines[i])-1 and wrapped_lines[i][j] == '\n'):
+                j = j-1 if boldEnd else j+1
+              #Add in bold
+              if(boldEnd):
+                wrapped_lines[i] = wrapped_lines[i][:j] + '\u001b[0m' + wrapped_lines[i][j:]
+              else:
+                wrapped_lines[i] = wrapped_lines[i][:j] + '\u001b[1m' + wrapped_lines[i][j:]
+              boldEnd = not boldEnd
+              break
+            else:
+              wrapped_lines[i] = wrapped_lines[i][:j] + '\u0332' + wrapped_lines[i][j:]
+          #Increase pos
+          if(wrapped_lines[i][j] != '\n'): #Disregard added newlines
+            pos += 1
+        
+    #Get rid of any white space after newline
+    for i in range(len(wrapped_lines)):
+      wrapped_lines[i] = wrapped_lines[i].replace('\n ', '\n')
+
+    #Combine list
     if(len(text) > 0 and text[-1] == '\n'):
       return '\n'.join(wrapped_lines) + '\n'
     return '\n'.join(wrapped_lines)
@@ -40,12 +92,15 @@ def input(prompt: object = ""):
 
 
 if __name__ == "__main__":
-  input("Hello:\n")
-  builtins.input("Hello:\n")
-  input("hi\n\n")
-  builtins.input("hi\n\n")
-  input('eep')
-  builtins.input('eep')
+  text = f"hello {underline('hel')+bold(underline('l'))+underline('o')} world {bold('world')}"
+  print(text)
+
+  #input("Hello:\n")
+  #builtins.input("Hello:\n")
+  #input("hi\n\n")
+  #uiltins.input("hi\n\n")
+  #input('eep')
+  #builtins.input('eep')
 
   print(f"WOAHHHHH {bold('BOLDING')}")
   print(f"HOLLOY COW {underline('WE UNDERLINE')}")
--- spotify_parsing/main.py
+++ spotify_parsing/main.py
@@ -1,6 +1,5 @@
 """
 TODO:
-  Finish wrapping texts
   Recolor Song Preference disabled
   Add podcast episode functionality (forceAdd done)
   Remove tokens if names come back as undefined
